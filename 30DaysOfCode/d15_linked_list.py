d46f7c08accbf53b05302767d8462a160c675866
John Portella <joportm@gmail.com>
2023-02-07 22:50:43 -0500

    Day 15 : Linked list

---
+++ 30DaysOfCode/d15_linked_list.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        if head is None:
+            return Node(data)
+        index = head
+        while index.next:
+            index = index.next
+        index.next = Node(data)
+        return head
+            
+        
+           
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
