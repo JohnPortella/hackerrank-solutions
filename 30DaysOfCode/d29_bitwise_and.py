f7cd02be6c7a9d96744278aefbf2b99ee9fad8e4
John Portella <joportm@gmail.com>
2023-02-21 12:30:51 -0500

    Solve day 29 bitwise and problem

---
+++ 30DaysOfCode/d29_bitwise_and.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bitwiseAnd' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER N
+#  2. INTEGER K
+#
+
+def bitwiseAnd(N, K):
+    # Write your code here
+    max_operator = -1
+    for a in range(K-2, N):
+        for b in range(a+1, N+1):
+            operator = a & b
+            if operator == K-1:
+                return operator
+            if operator < K and operator > max_operator:
+                max_operator = operator
+    return max_operator
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        count = int(first_multiple_input[0])
+
+        lim = int(first_multiple_input[1])
+
+        res = bitwiseAnd(count, lim)
+
+        fptr.write(str(res) + '\n')
+
+    fptr.close()
